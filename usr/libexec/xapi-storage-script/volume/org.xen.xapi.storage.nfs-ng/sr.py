5fb7246f80480432b52c47ad233d8635c7611892
Ronan Abhamon <ronan.abhamon@vates.fr>
2019-09-10 10:28:32 +0200

    fix(nfs-ng plugin): fix coding style + add set_description/name methods + use new api

--- usr/libexec/xapi-storage-script/volume/org.xen.xapi.storage.nfs-ng/sr.py
+++ usr/libexec/xapi-storage-script/volume/org.xen.xapi.storage.nfs-ng/sr.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
-from __future__ import division
 import os
 import os.path
 import sys
 import errno
 import urlparse
-import json
 
 import xapi.storage.api.v5.volume
-from xapi.storage.common import call
-from xapi.storage.libs.libcow.volume import COWVolume
-from xapi.storage.libs.libcow.coalesce import COWCoalesce
 from xapi.storage import log
+from xapi.storage.common import call
+from xapi.storage.libs import util
+from xapi.storage.libs.libcow.coalesce import COWCoalesce
+from xapi.storage.libs.libcow.volume import COWVolume
 
 import importlib
 
 
+@util.decorate_all_routines(util.log_exceptions_in_function)
 class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
-
     # Base of SR mounts, SRs will mount in a folder with their GUID
-    MOUNTPOINT_ROOT = "/var/run/sr-mount/"
+    MOUNTPOINT_ROOT = '/var/run/sr-mount/'
 
-    def _mount_path(self, sr_uuid):
-        return os.path.abspath(self.MOUNTPOINT_ROOT + str(sr_uuid))
+    @classmethod
+    def _mount_path(cls, sr_uuid):
+        return os.path.abspath(cls.MOUNTPOINT_ROOT + str(sr_uuid))
 
     def _mount(self, dbg, nfs_path, sr_uuid):
         """Mount the NFS share to a temporary local folder"""
@@ -36,37 +36,34 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
             else:
                 raise
         if not os.path.ismount(mnt_path):
-            cmd = ["/usr/bin/mount", "-t", "nfs", "-o",
-                   "noatime,nodiratime", nfs_path, mnt_path]
+            cmd = ['/usr/bin/mount', '-t', 'nfs', '-o',
+                   'noatime,nodiratime', nfs_path, mnt_path]
             call(dbg, cmd)
         return mnt_path
 
-    def _umount(self, dbg, mnt_path):
+    def _unmount(self, dbg, mnt_path):
         """Unmount the NFS share"""
-        cmd = ["/usr/bin/umount", mnt_path]
+        cmd = ['/usr/bin/umount', mnt_path]
         call(dbg, cmd)
 
     def probe(self, dbg, configuration):
-        uris = []
-        srs = []
         uri = configuration['uri']
         u = urlparse.urlparse(uri)
         if u.scheme is None:
             raise xapi.storage.api.v5.volume.SR_does_not_exist(
-                "The SR URI is invalid")
+                'The SR URI is invalid')
 
-        raise xapi.storage.api.v5.volume.Unimplemented("probe")
+        raise xapi.storage.api.v5.volume.Unimplemented('probe')
 
         return {
-            "srs": srs,
-            "uris": uris
+            'srs': [],
+            'uris': []
         }
 
     def attach(self, dbg, configuration):
         uri = configuration['uri']
-        sr_uuid = configuration['sr_uuid']
-
-        log.debug("%s: SR.attach: uri=%s" % (dbg, uri))
+        log.debug('{}: SR.attach: config={}, uri={}'.format(
+            dbg, configuration, uri))
 
         nfs_uri = urlparse.urlsplit(uri)
         if nfs_uri.scheme != 'nfs':
@@ -74,6 +71,7 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
 
         nfs_server = '{0}:{1}'.format(nfs_uri.netloc, nfs_uri.path)
 
+        sr_uuid = configuration['sr_uuid']
         mnt_path = self._mount_path(sr_uuid)
         sr_dir = os.path.join(mnt_path, sr_uuid)
         sr = urlparse.urlunsplit(('file', '', sr_dir, None, None))
@@ -90,13 +88,13 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
             raise ValueError('SR directory doesn\'t exist')
 
         # Start GC for this host
-        COWCoalesce.start_gc(dbg, "nfs-ng", sr)
+        COWCoalesce.start_gc(dbg, 'nfs-ng', sr)
 
         return sr
 
     def create(self, dbg, sr_uuid, configuration, name, description):
-        log.debug("%s: SR.create: config=%s" %
-                  (dbg, configuration))
+        log.debug('{}: SR.create: config={}, sr_uuid={}'.format(
+            dbg, configuration, sr_uuid))
 
         uri = configuration['uri']
         nfs_uri = urlparse.urlsplit(uri)
@@ -108,11 +106,11 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
         # Temporarily mount the filesystem so we can write the SR metadata
         mnt_path = self._mount(dbg, nfs_server, sr_uuid)
 
-        sr_path = os.path.join(mnt_path, str(sr_uuid))
+        sr = os.path.join(mnt_path, str(sr_uuid))
 
         # Create SR folder based on name
         try:
-            os.makedirs(sr_path)
+            os.makedirs(sr)
         except OSError as exc:
             if exc.errno == errno.EEXIST:
                 # Need a specifc error here, SR_already_exists.
@@ -122,7 +120,7 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
                 raise
 
         # Create the metadata database
-        importlib.import_module("nfs-ng").Callbacks().create_database(sr_path)
+        importlib.import_module('nfs-ng').Callbacks().create_database(sr)
 
         read_caching = True
         if 'read_caching' in configuration:
@@ -130,25 +128,19 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
                     'true', 't', 'on', '1', 'yes']:
                 read_caching = False
 
-        configuration['sr_uuid'] = sr_uuid
-
         meta = {
-            "name": name,
-            "description": description,
-            "uri": uri,
-            "unique_id": sr_uuid,
-            "read_caching": read_caching,
-            "keys": {}
+            'name': name,
+            'description': description,
+            'uri': uri,
+            'unique_id': sr_uuid,
+            'read_caching': read_caching,
+            'keys': {}
         }
-        metapath = sr_path + "/meta.json"
-        log.debug("%s: dumping metadata to %s: %s" % (dbg, metapath, meta))
+        util.update_sr_metadata(dbg, 'file://' + sr, meta)
 
-        with open(metapath, "w") as json_fp:
-            json.dump(meta, json_fp)
-            json_fp.write("\n")
+        self._unmount(dbg, mnt_path)
 
-        self._umount(dbg, mnt_path)
-
+        configuration['sr_uuid'] = sr_uuid
         return configuration
 
     def destroy(self, dbg, sr):
@@ -158,55 +150,56 @@ class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
     def detach(self, dbg, sr):
         # stop GC
         try:
-            COWCoalesce.stop_gc(dbg, "nfs-ng", sr)
+            COWCoalesce.stop_gc(dbg, 'nfs-ng', sr)
         except:
-            log.debug("GC already stopped")
+            log.debug('GC already stopped')
 
         # Unmount the FS
         sr_path = urlparse.urlparse(sr).path
         mnt_path = os.path.dirname(sr_path)
-        self._umount(dbg, mnt_path)
+        self._unmount(dbg, mnt_path)
         os.rmdir(mnt_path)
 
     def ls(self, dbg, sr):
-        raise xapi.storage.api.v5.volume.Unimplemented("ls")
+        return COWVolume.ls(
+            dbg, sr, importlib.import_module('nfs-ng').Callbacks())
+
+    def set_description(self, dbg, sr, new_description):
+        util.update_sr_metadata(
+            dbg, 'file://' + sr, {'description': new_description})
+
+    def set_name(self, dbg, sr, new_name):
+        util.update_sr_metadata(dbg, 'file://' + sr, {'name': new_name})
 
     def stat(self, dbg, sr):
-        # SR path (sr) is file://<mnt_path>
-        # Get mnt_path by dropping url scheme
-        sr_path = urlparse.urlparse(sr).path
-        mnt_path = os.path.dirname(sr_path)
-
-        if not(os.path.isdir(mnt_path)) or not os.path.ismount(mnt_path):
-            raise xapi.storage.api.v5.volume.Sr_not_attached(mnt_path)
+        if not os.path.isdir(sr) or not os.path.ismount(sr):
+            raise xapi.storage.api.v5.volume.Sr_not_attached(sr)
 
         # Get the filesystem size
-        statvfs = os.statvfs(mnt_path)
+        statvfs = os.statvfs(sr)
         psize = statvfs.f_blocks * statvfs.f_frsize
         fsize = statvfs.f_bfree * statvfs.f_frsize
-        log.debug("%s: statvfs says psize = %Ld" % (dbg, psize))
-
-        nfs = importlib.import_module("nfs-ng")
+        log.debug('{}: statvfs says psize = {}'.format(dbg, psize))
 
         overprovision = COWVolume.get_sr_provisioned_size(
-            sr, nfs.Callbacks()) / psize
+            sr, importlib.import_module('nfs-ng').Callbacks()) / psize
 
+        meta = util.get_sr_metadata(dbg, 'file://' + sr)
         return {
-            "sr": sr,
-            "name": "SR Name",
-            "description": "NFS SR",
-            "uuid": os.path.basename(sr_path),
-            "total_space": psize,
-            "free_space": fsize,
-            "overprovision": overprovision,
-            "datasources": [],
-            "clustered": True,
-            "health": ["Healthy", ""]
+            'sr': sr,
+            'name': meta['name'],
+            'description': meta['description'],
+            'total_space': psize,
+            'free_space': fsize,
+            'uuid': meta['unique_id'],
+            'overprovision': overprovision,
+            'datasources': [],
+            'clustered': True,
+            'health': ['Healthy', '']
         }
 
 
-def call_sr_command():
-    """Process the command line arguments and call the required operation"""
+if __name__ == '__main__':
     log.log_call_argv()
     cmd = xapi.storage.api.v5.volume.SR_commandline(Implementation())
     base = os.path.basename(sys.argv[0])
@@ -222,11 +215,11 @@ def call_sr_command():
         cmd.detach()
     elif base == 'SR.ls':
         cmd.ls()
+    elif base == 'SR.set_description':
+        cmd.set_description()
+    elif base == 'SR.set_name':
+        cmd.set_name()
     elif base == 'SR.stat':
         cmd.stat()
     else:
         raise xapi.storage.api.v5.volume.Unimplemented(base)
-
-
-if __name__ == "__main__":
-    call_sr_command()
