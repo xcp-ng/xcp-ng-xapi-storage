0cd2522f4546813b7c930f8f9ad670b9bc5b60bf
Ronan Abhamon <ronan.abhamon@vates.fr>
2019-09-11 13:39:00 +0200

    feat(xcp-ng-xapi-storage): add a build system

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='xcp-ng-xapi-storage-libs',
+    version='1.0.0',
+    description='XCP-ng implementation of the xapi-storage interface',
+    author='XCP-ng team',
+    author_email='contact@xcp-ng.com',
+    url='https://github.com/xcp-ng/xcp-ng-xapi-storage',
+    license='LGPLv2.1',
+    packages=[
+        'xapi.storage.libs.libcow',
+        'xapi.storage.libs.rrddlib'
+    ]
+)
