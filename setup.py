84416a1b64ed1a69d69cee23992fcc68e405482c
Ronan Abhamon <ronan.abhamon@vates.fr>
2019-09-17 14:56:40 +0200

    chore(CMakeLists.txt): update version (1.0.2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='xcp-ng-xapi-storage-libs',
-    version='1.0.1',
+    version='1.0.2',
     description='XCP-ng implementation of the xapi-storage interface',
     author='XCP-ng team',
     author_email='contact@xcp-ng.com',
