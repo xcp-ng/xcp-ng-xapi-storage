2d9c8a7a4db3db2b4714280e3d2761c969c44dee
Yann Dirson <yann.dirson@vates.tech>
2024-03-14 19:56:27 +0100

    zfs-ng: base infrastructure
    Bare minimum for a plugin, no operation implemented.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

---
+++ plugins/volume/org.xen.xapi.storage.zfs-ng/sr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import importlib
+import os
+import os.path
+import sys
+import urlparse
+
+from xapi.storage import log
+from xapi.storage.libs import util
+from xapi.storage.common import call
+from xapi.storage.libs.libcow.volume import COWVolume
+import xapi.storage.api.v5.volume
+
+
+@util.decorate_all_routines(util.log_exceptions_in_function)
+class Implementation(xapi.storage.api.v5.volume.SR_skeleton):
+    "SR driver to provide volumes from zvol's"
+
+if __name__ == '__main__':
+    log.log_call_argv()
+    cmd = xapi.storage.api.v5.volume.SR_commandline(Implementation())
+    base = os.path.basename(sys.argv[0])
+    raise xapi.storage.api.v5.volume.Unimplemented(base)
---
+++ plugins/volume/org.xen.xapi.storage.zfs-ng/zfs-ng.py
@@ -0,0 +1,5 @@
+import os.path
+import xapi.storage.libs.libcow.callbacks
+
+class Callbacks(xapi.storage.libs.libcow.callbacks.Callbacks):
+    "ZFS-ng callbacks"
